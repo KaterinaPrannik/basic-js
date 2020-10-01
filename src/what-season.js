e9e9a87beb839fd7d695984101b6c3a7ccc352e6
KaterinaPrannik <60596523+KaterinaPrannik@users.noreply.github.com>
2020-10-01 22:21:35 +0300

    fix: decide tasks cats and what-seasons

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if(date===undefined){
+    return "Unable to determine the time of year!"
+  } else if (date instanceof Date && !isNaN(date)){
+    let  month=date.getMonth();
+     let season;
+
+     if(month==11|| month>=0 && month<=1){
+       season="winter";
+     }
+     else if(month>=2 && month<=4){
+       season="spring"
+     } else if(month>=5 && month<=7){
+       season="summer";
+     } else if(month>=8  && month<=10){
+       season="autumn"
+     }
+return season;
+  }
+  else if(date instanceof !Date){
+    return Error;
+  }
+}
